54bb59c4c9940bccb66a5ece973e672916412d19
Emon Nath <138616621+Emondevnath@users.noreply.github.com>
2024-08-11 19:39:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// light on of function
+function showOff(){
+    document.getElementById('lightImg').src ="../images/ofbulb.gif";
+}
+function showOn(){
+    document.getElementById('lightImg').src ="../images/onbulb.gif";
+}
+
+
+// light on of function
+
+// feeling sad smile
+function sadOff(){
+    document.getElementById('felImg').src ="../images/sad.png";
+}
+function smileOn(){
+    document.getElementById('felImg').src ="../images/smile.png";
+}
+// feeling sad smile
+
+
+// diamond ruby
+function felOff(){
+    document.getElementById('drImg').src ="../images/ruby.png";
+}
+function felOn(){
+    document.getElementById('drImg').src ="../images/diamond.png";
+}
+// diamond ruby
+
+// increase font
+function showFont(){
+  document.getElementById('fSize').style.fontSize = "40px" ;
+}
+// increase font
+
+// chance color
+function showColor(){
+    document.getElementById('fColor').style.color = "red" ;
+  }
+  // chance color
